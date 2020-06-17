ccc6a3befd33d6001b46394efb97a5f4c124c4be
Sarveswara rao <rao.sarveswara@gmail.com>
2020-06-17 17:57:25 +1000

    new component

---
+++ force-app/main/default/lwc/createContactEdView/createContactEdView.js
@@ -0,0 +1,10 @@
+import { LightningElement,track } from 'lwc';
+
+export default class CreateContactEdView extends LightningElement {
+    @track recordId;
+
+    createContact(event){
+      this.recordId = event.detail.id;
+    }
+  
+}
